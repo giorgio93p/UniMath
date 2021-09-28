6d0e03e5d8953d85470ccb5a66a8e1ff43d9a271
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2021-09-28 14:18:37 +0300

    Add a correct definition and a notation for satisfaction. Use an alias for the type of signatures.

--- UniMath/InstitutionTheory/test.v
+++ UniMath/InstitutionTheory/test.v
@@ -9,21 +9,28 @@ Require Import UniMath.CategoryTheory.opp_precat.
 Require Import UniMath.CategoryTheory.All.
 
 Local Open Scope cat .
+Declare Scope institutions.
+Local Open Scope institutions.
 
 Section def_Institution.
-  Definition sents_of (sign : category) : UU :=
+  Definition signature : UU := category .
+
+  Definition signature_to_category (sign : signature) : category := sign .
+  Coercion signature_to_category : signature >-> category .
+
+  Definition sents_of (sign : signature)  : UU :=
     functor sign type_precat . (** for some reason, sign → SET fails below; do we need it however? **)
 
-  Definition mods_of (sign: category) : UU :=
+  Definition mods_of (sign: signature) : UU :=
     functor (oppositeCategory sign) type_precat .
 
-  Definition satisfaction_of (sign : category) (sent : sents_of sign) (mod : oppositeCategory sign ⟶ type_precat) : UU :=
+  Definition satisfaction_of (sign : signature) (sent : sents_of sign) (mod : oppositeCategory sign ⟶ type_precat) : UU :=
     ∏ Σ : sign, mod Σ -> (pr1 sent) Σ -> bool .
 
   Definition institution_data : UU
-    := ∑ (sign : category), ∑ (sent : sents_of sign), ∑ (mod: mods_of sign), satisfaction_of sign sent mod  .
+    := ∑ (sign : signature), ∑ (sent : sents_of sign), ∑ (mod: mods_of sign), satisfaction_of sign sent mod  .
 
-  Definition make_institution_data (sign : category) (sent : sents_of sign) (mod: mods_of sign) (rel : satisfaction_of sign sent mod) : institution_data
+  Definition make_institution_data (sign : signature) (sent : sents_of sign) (mod: mods_of sign) (rel : satisfaction_of sign sent mod) : institution_data
     := sign ,, sent ,, mod ,,  rel .
 
   Definition sign (I : institution_data) : category := pr1 I.
@@ -31,22 +38,22 @@ Section def_Institution.
   Definition sent (I : institution_data) : sents_of (sign I) := pr12 I.
   Definition satisfaction (I : institution_data) : satisfaction_of (sign I) (sent I) (mod I) := pr222 I.
 
+  Definition satisfies {I:institution_data} {Σ : sign I} (M : pr1 (mod I) Σ) (φ : pr1 (sent I) Σ) := satisfaction I Σ M φ .
+  Notation "M ⊧ φ" := (satisfies M φ) (at level 100) : institutions.
+
   Definition is_institution (I : institution_data) : UU
     :=
       ∏ (Σ Σ' : sign I) (σ : Σ --> Σ'),
       ∏ (M' : pr1 (mod I) Σ'),
       ∏ φ : pr1 (sent I) Σ,
-            satisfaction I Σ' M' (#(pr1 (sent I)) σ φ) = satisfaction I Σ (#(pr1 (mod I)) σ M') φ.
+            satisfies M' (#(pr1 (sent I)) σ φ) = satisfies (#(pr1 (mod I)) σ M') φ.
+
+  (* Mossakowski: We assume that all institutions are such that satisfaction is
+invariant under model isomorphism, i.e., if Σ-models M, M' are isomorphic, then
+M ⊧ φ iff M' ⊧ φ for all Σ-sentences φ. *)
 
   Definition institution := total2 is_institution .
 
   Definition institution_to_institution_data (I : institution) : institution_data := pr1 I.
   Coercion institution_to_institution_data : institution >-> institution_data.
 End def_Institution.
-
-(**
-Section satisfaction .
-
-  Definition satisfies {I:institution} {Σ : sign I} (M : pr1 (mod I) Σ) (φ : Σ) := satisfaction I Σ M φ .
-End satisfaction.
-**)
